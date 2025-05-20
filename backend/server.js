a985bb9bec28c449d832e7e075bb737a804a1218
TamizhSK <tamizhsk1@gmail.com>
2025-05-21 03:40:26 +0530

    chore : important update

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const app = express();
 
 // CORS configuration
 const corsOptions = {
-  origin: "https://donezo-vert.vercel.app",
+  origin: "https://donezo-vert.vercel.app/",
   methods: "GET,POST,PUT,DELETE",
   credentials: true,
 };
