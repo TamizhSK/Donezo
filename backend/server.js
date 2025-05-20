0fd7eb72f095b0d432371bfac81491c60632b8a9
TamizhSK <tamizhsk1@gmail.com>
2025-05-21 02:33:33 +0530

    chore : major change

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ const db = mysql.createConnection({
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE,
-  port: process.env.MYSQL_PORT || 5000,
+  port: process.env.MYSQL_PORT,
 });
 
 db.connect((err) => {
